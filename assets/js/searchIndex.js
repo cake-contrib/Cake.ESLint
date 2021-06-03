0b0c011e9fca4c560c1bdb4a0696ad51e9528920
GitHub <noreply@github.com>
2021-06-03 21:25:51 +0000

    Continuous Integration Publish: e394506fdc48c47a65915fc9b8c88810ac39eff4 Merge pull request #26 from cake-contrib/renovate/verify.xunit-11.x
    (maint) Update dependency Verify.Xunit to v11.18.2

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -53,34 +53,6 @@ var searchModule = function() {
     a(
         {
             id:1,
-            title:"ESLintSettings",
-            content:"ESLintSettings",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettings',
-            title:"ESLintSettings",
-            description:""
-        }
-    );
-    a(
-        {
-            id:2,
-            title:"ESLintSettingsExtensions",
-            content:"ESLintSettingsExtensions",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettingsExtensions',
-            title:"ESLintSettingsExtensions",
-            description:""
-        }
-    );
-    a(
-        {
-            id:3,
             title:"ESLintAliases",
             content:"ESLintAliases",
             description:'',
@@ -94,7 +66,21 @@ var searchModule = function() {
     );
     a(
         {
-            id:4,
+            id:2,
+            title:"ESLintSettings",
+            content:"ESLintSettings",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettings',
+            title:"ESLintSettings",
+            description:""
+        }
+    );
+    a(
+        {
+            id:3,
             title:"ESLintRunner",
             content:"ESLintRunner",
             description:'',
@@ -103,6 +89,20 @@ var searchModule = function() {
         {
             url:'/Cake.ESLint/api/Cake.ESLint/ESLintRunner',
             title:"ESLintRunner",
+            description:""
+        }
+    );
+    a(
+        {
+            id:4,
+            title:"ESLintSettingsExtensions",
+            content:"ESLintSettingsExtensions",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettingsExtensions',
+            title:"ESLintSettingsExtensions",
             description:""
         }
     );
