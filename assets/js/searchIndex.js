0450096f38a177e1b489c2b77a350894ec862548
GitHub <noreply@github.com>
2021-11-09 06:51:33 +0000

    Continuous Integration Publish: 1aa4a3a468e725b732c7fc767f7f3a97544b5f08 Merge pull request #105 from cake-contrib/renovate/microsoft.sourcelink.github-1.x
    (maint) Update dependency Microsoft.SourceLink.GitHub to v1.1.0

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -39,34 +39,20 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"ESLintSettings",
-            content:"ESLintSettings",
+            title:"ESLintOutputFormat",
+            content:"ESLintOutputFormat",
             description:'',
             tags:''
         },
         {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettings',
-            title:"ESLintSettings",
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintOutputFormat',
+            title:"ESLintOutputFormat",
             description:""
         }
     );
     a(
         {
             id:1,
-            title:"ESLintSettingsExtensions",
-            content:"ESLintSettingsExtensions",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettingsExtensions',
-            title:"ESLintSettingsExtensions",
-            description:""
-        }
-    );
-    a(
-        {
-            id:2,
             title:"ESLintFixType",
             content:"ESLintFixType",
             description:'',
@@ -80,21 +66,21 @@ var searchModule = function() {
     );
     a(
         {
-            id:3,
-            title:"ESLintRunner",
-            content:"ESLintRunner",
+            id:2,
+            title:"ESLintSettings",
+            content:"ESLintSettings",
             description:'',
             tags:''
         },
         {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintRunner',
-            title:"ESLintRunner",
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettings',
+            title:"ESLintSettings",
             description:""
         }
     );
     a(
         {
-            id:4,
+            id:3,
             title:"ESLintAliases",
             content:"ESLintAliases",
             description:'',
@@ -103,6 +89,20 @@ var searchModule = function() {
         {
             url:'/Cake.ESLint/api/Cake.ESLint/ESLintAliases',
             title:"ESLintAliases",
+            description:""
+        }
+    );
+    a(
+        {
+            id:4,
+            title:"ESLintRunner",
+            content:"ESLintRunner",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintRunner',
+            title:"ESLintRunner",
             description:""
         }
     );
@@ -123,14 +123,14 @@ var searchModule = function() {
     a(
         {
             id:6,
-            title:"ESLintOutputFormat",
-            content:"ESLintOutputFormat",
+            title:"ESLintSettingsExtensions",
+            content:"ESLintSettingsExtensions",
             description:'',
             tags:''
         },
         {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintOutputFormat',
-            title:"ESLintOutputFormat",
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettingsExtensions',
+            title:"ESLintSettingsExtensions",
             description:""
         }
     );
