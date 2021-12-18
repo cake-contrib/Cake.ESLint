3cd030bc27fa22440fba5297e9c38d25820e4fb9
GitHub <noreply@github.com>
2021-12-18 22:24:53 +0000

    Continuous Integration Publish: c822fa1bad23169ca822ff062a88c1f39f76e3b2 Merge pull request #136 from cake-contrib/renovate/verify.xunit-14.x
    (maint) Update dependency Verify.Xunit to v14.11.2

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -53,34 +53,6 @@ var searchModule = function() {
     a(
         {
             id:1,
-            title:"ESLintSettings",
-            content:"ESLintSettings",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettings',
-            title:"ESLintSettings",
-            description:""
-        }
-    );
-    a(
-        {
-            id:2,
-            title:"ESLintRunner",
-            content:"ESLintRunner",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintRunner',
-            title:"ESLintRunner",
-            description:""
-        }
-    );
-    a(
-        {
-            id:3,
             title:"ESLintSettingsExtensions",
             content:"ESLintSettingsExtensions",
             description:'',
@@ -94,7 +66,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:4,
+            id:2,
             title:"ESLintFixType",
             content:"ESLintFixType",
             description:'',
@@ -108,7 +80,21 @@ var searchModule = function() {
     );
     a(
         {
-            id:5,
+            id:3,
+            title:"ESLintCacheStrategy",
+            content:"ESLintCacheStrategy",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintCacheStrategy',
+            title:"ESLintCacheStrategy",
+            description:""
+        }
+    );
+    a(
+        {
+            id:4,
             title:"ESLintOutputFormat",
             content:"ESLintOutputFormat",
             description:'',
@@ -122,15 +108,29 @@ var searchModule = function() {
     );
     a(
         {
-            id:6,
-            title:"ESLintCacheStrategy",
-            content:"ESLintCacheStrategy",
+            id:5,
+            title:"ESLintRunner",
+            content:"ESLintRunner",
             description:'',
             tags:''
         },
         {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintCacheStrategy',
-            title:"ESLintCacheStrategy",
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintRunner',
+            title:"ESLintRunner",
+            description:""
+        }
+    );
+    a(
+        {
+            id:6,
+            title:"ESLintSettings",
+            content:"ESLintSettings",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettings',
+            title:"ESLintSettings",
             description:""
         }
     );
