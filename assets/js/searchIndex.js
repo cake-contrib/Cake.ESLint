dfdc9ce2c44ec70ce003cfe26d8e0d488d295090
GitHub <noreply@github.com>
2022-03-07 07:47:48 +0000

    Continuous Integration Publish: f26e549a59f5a5fe4d5afde03c506029e5a2e2ff Merge pull request #176 from cake-contrib/renovate/verify.xunit-16.x
    (maint) Update dependency Verify.Xunit to v16.3.5

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -67,20 +67,6 @@ var searchModule = function() {
     a(
         {
             id:2,
-            title:"ESLintSettings",
-            content:"ESLintSettings",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettings',
-            title:"ESLintSettings",
-            description:""
-        }
-    );
-    a(
-        {
-            id:3,
             title:"ESLintRunner",
             content:"ESLintRunner",
             description:'',
@@ -94,7 +80,35 @@ var searchModule = function() {
     );
     a(
         {
+            id:3,
+            title:"ESLintFixType",
+            content:"ESLintFixType",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintFixType',
+            title:"ESLintFixType",
+            description:""
+        }
+    );
+    a(
+        {
             id:4,
+            title:"ESLintSettings",
+            content:"ESLintSettings",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettings',
+            title:"ESLintSettings",
+            description:""
+        }
+    );
+    a(
+        {
+            id:5,
             title:"ESLintSettingsExtensions",
             content:"ESLintSettingsExtensions",
             description:'',
@@ -108,7 +122,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:5,
+            id:6,
             title:"ESLintAliases",
             content:"ESLintAliases",
             description:'',
@@ -117,20 +131,6 @@ var searchModule = function() {
         {
             url:'/Cake.ESLint/api/Cake.ESLint/ESLintAliases',
             title:"ESLintAliases",
-            description:""
-        }
-    );
-    a(
-        {
-            id:6,
-            title:"ESLintFixType",
-            content:"ESLintFixType",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintFixType',
-            title:"ESLintFixType",
             description:""
         }
     );
