a77a7ad663a28650133ca86e933bbbecdecca4b7
GitHub <noreply@github.com>
2022-08-31 11:13:54 +0000

    Continuous Integration Publish: b610f8ce5e51176948ada71d60a3404783862143 Merge pull request #242 from cake-contrib/renovate/vstest-monorepo
    (maint) Update dependency Microsoft.NET.Test.Sdk to v17.3.1

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -39,34 +39,6 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"ESLintSettingsExtensions",
-            content:"ESLintSettingsExtensions",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettingsExtensions',
-            title:"ESLintSettingsExtensions",
-            description:""
-        }
-    );
-    a(
-        {
-            id:1,
-            title:"ESLintCacheStrategy",
-            content:"ESLintCacheStrategy",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintCacheStrategy',
-            title:"ESLintCacheStrategy",
-            description:""
-        }
-    );
-    a(
-        {
-            id:2,
             title:"ESLintRunner",
             content:"ESLintRunner",
             description:'',
@@ -80,21 +52,35 @@ var searchModule = function() {
     );
     a(
         {
-            id:3,
-            title:"ESLintOutputFormat",
-            content:"ESLintOutputFormat",
+            id:1,
+            title:"ESLintSettingsExtensions",
+            content:"ESLintSettingsExtensions",
             description:'',
             tags:''
         },
         {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintOutputFormat',
-            title:"ESLintOutputFormat",
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettingsExtensions',
+            title:"ESLintSettingsExtensions",
             description:""
         }
     );
     a(
         {
-            id:4,
+            id:2,
+            title:"ESLintCacheStrategy",
+            content:"ESLintCacheStrategy",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintCacheStrategy',
+            title:"ESLintCacheStrategy",
+            description:""
+        }
+    );
+    a(
+        {
+            id:3,
             title:"ESLintFixType",
             content:"ESLintFixType",
             description:'',
@@ -103,6 +89,20 @@ var searchModule = function() {
         {
             url:'/Cake.ESLint/api/Cake.ESLint/ESLintFixType',
             title:"ESLintFixType",
+            description:""
+        }
+    );
+    a(
+        {
+            id:4,
+            title:"ESLintOutputFormat",
+            content:"ESLintOutputFormat",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintOutputFormat',
+            title:"ESLintOutputFormat",
             description:""
         }
     );
