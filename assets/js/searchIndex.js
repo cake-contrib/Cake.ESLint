600ec9de5aca7200595a1e80aa6ee91b9a399db1
GitHub <noreply@github.com>
2022-11-30 09:35:16 +0000

    Continuous Integration Publish: 259e319f41740405a6c24d0ae7c042a8d90e1b3c Merge pull request #279 from cake-contrib/renovate/verify.xunit-19.x
    (maint) Update dependency Verify.Xunit to v19.3.0

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -39,34 +39,6 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"ESLintCacheStrategy",
-            content:"ESLintCacheStrategy",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintCacheStrategy',
-            title:"ESLintCacheStrategy",
-            description:""
-        }
-    );
-    a(
-        {
-            id:1,
-            title:"ESLintSettingsExtensions",
-            content:"ESLintSettingsExtensions",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettingsExtensions',
-            title:"ESLintSettingsExtensions",
-            description:""
-        }
-    );
-    a(
-        {
-            id:2,
             title:"ESLintOutputFormat",
             content:"ESLintOutputFormat",
             description:'',
@@ -80,21 +52,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:3,
-            title:"ESLintSettings",
-            content:"ESLintSettings",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettings',
-            title:"ESLintSettings",
-            description:""
-        }
-    );
-    a(
-        {
-            id:4,
+            id:1,
             title:"ESLintFixType",
             content:"ESLintFixType",
             description:'',
@@ -108,7 +66,21 @@ var searchModule = function() {
     );
     a(
         {
-            id:5,
+            id:2,
+            title:"ESLintCacheStrategy",
+            content:"ESLintCacheStrategy",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintCacheStrategy',
+            title:"ESLintCacheStrategy",
+            description:""
+        }
+    );
+    a(
+        {
+            id:3,
             title:"ESLintRunner",
             content:"ESLintRunner",
             description:'',
@@ -122,7 +94,21 @@ var searchModule = function() {
     );
     a(
         {
-            id:6,
+            id:4,
+            title:"ESLintSettingsExtensions",
+            content:"ESLintSettingsExtensions",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettingsExtensions',
+            title:"ESLintSettingsExtensions",
+            description:""
+        }
+    );
+    a(
+        {
+            id:5,
             title:"ESLintAliases",
             content:"ESLintAliases",
             description:'',
@@ -131,6 +117,20 @@ var searchModule = function() {
         {
             url:'/Cake.ESLint/api/Cake.ESLint/ESLintAliases',
             title:"ESLintAliases",
+            description:""
+        }
+    );
+    a(
+        {
+            id:6,
+            title:"ESLintSettings",
+            content:"ESLintSettings",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettings',
+            title:"ESLintSettings",
             description:""
         }
     );
