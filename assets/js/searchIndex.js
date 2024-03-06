0b3bd24c566b7ff8ffb0e084a15a5ded0dbef3c0
GitHub <noreply@github.com>
2024-03-06 16:36:58 +0000

    Continuous Integration Publish: 46328e438c351eb5829fca3990f57d64fd649afd Merge pull request #445 from cake-contrib/renovate/verify.xunit-23.x
    (maint) Update dependency Verify.Xunit to v23.2.3

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -39,14 +39,14 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"ESLintRunner",
-            content:"ESLintRunner",
+            title:"ESLintAliases",
+            content:"ESLintAliases",
             description:'',
             tags:''
         },
         {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintRunner',
-            title:"ESLintRunner",
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintAliases',
+            title:"ESLintAliases",
             description:""
         }
     );
@@ -81,20 +81,6 @@ var searchModule = function() {
     a(
         {
             id:3,
-            title:"ESLintSettings",
-            content:"ESLintSettings",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettings',
-            title:"ESLintSettings",
-            description:""
-        }
-    );
-    a(
-        {
-            id:4,
             title:"ESLintFixType",
             content:"ESLintFixType",
             description:'',
@@ -108,21 +94,21 @@ var searchModule = function() {
     );
     a(
         {
-            id:5,
-            title:"ESLintAliases",
-            content:"ESLintAliases",
+            id:4,
+            title:"ESLintSettings",
+            content:"ESLintSettings",
             description:'',
             tags:''
         },
         {
-            url:'/Cake.ESLint/api/Cake.ESLint/ESLintAliases',
-            title:"ESLintAliases",
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintSettings',
+            title:"ESLintSettings",
             description:""
         }
     );
     a(
         {
-            id:6,
+            id:5,
             title:"ESLintOutputFormat",
             content:"ESLintOutputFormat",
             description:'',
@@ -131,6 +117,20 @@ var searchModule = function() {
         {
             url:'/Cake.ESLint/api/Cake.ESLint/ESLintOutputFormat',
             title:"ESLintOutputFormat",
+            description:""
+        }
+    );
+    a(
+        {
+            id:6,
+            title:"ESLintRunner",
+            content:"ESLintRunner",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.ESLint/api/Cake.ESLint/ESLintRunner',
+            title:"ESLintRunner",
             description:""
         }
     );
